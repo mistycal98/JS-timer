9634841bf72a2830fcd4280a793d4e0ce19bf247
Tushar Mistry <mistry.tushar98@gmail.com>
2020-11-28 17:08:59 +0530

    Added minor Styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('starting Script.....');
+console.log("starting Script.....");
 
 let hours = 0;
 let minutes = 0;
@@ -13,59 +13,61 @@ let interval = 0;
 let status = "stop";
 
 function start() {
+  seconds++;
 
-    seconds++;
-
-    if (seconds / 60 === 1) {
-        seconds = 0;
-        minutes++;
-        if (minutes / 60 === 1) {
-            minutes = 0;
-            hours++;
-        }
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
     }
+  }
 
-    if (minutes < 10) {
-        minute = "0" + minutes.toString();
-    } else {
-        minute = minutes;
-    }
+  if (minutes < 10) {
+    minute = "0" + minutes.toString();
+  } else {
+    minute = minutes;
+  }
 
-    if (seconds < 10) {
-        second = "0" + seconds.toString();
-    } else {
-        second = seconds;
-    }
+  if (seconds < 10) {
+    second = "0" + seconds.toString();
+  } else {
+    second = seconds;
+  }
 
-    if (hours < 10) {
-        hour = "0" + hours.toString();
-    } else {
-        hour = hours;
-    }
+  if (hours < 10) {
+    hour = "0" + hours.toString();
+  } else {
+    hour = hours;
+  }
 
-    document.getElementById('timer').innerHTML = hour + ":" + minute + ":" + second;
+  document.getElementById(
+    "timer"
+  ).innerHTML = `${hour}<span id="hour">H </span>${minute}<span id="min" >M </span><span id="sec">${second}<span>S </span></span>`;
 }
 
 function startStop() {
-    if (status === 'stop') {
+  if (status === "stop") {
+    interval = window.setInterval(start, 1);
+    document.getElementById("startStop").innerHTML =
+      '<i class="fas fa-pause fa-4x"></i>';
+    status = "start";
+  } else {
+    window.clearInterval(interval);
+    document.getElementById("startStop").innerHTML =
+      '<i class="fas fa-play fa-4x"></i>';
 
-        interval = window.setInterval(start, 1000);
-        document.getElementById('startStop').innerHTML = '<i class="fas fa-pause fa-2x"></i>';
-        status = 'start';
-
-    } else {
-        window.clearInterval(interval);
-        document.getElementById('startStop').innerHTML = '<i class="fas fa-play fa-2x"></i>';
-
-        status = 'stop';
-    }
+    status = "stop";
+  }
 }
 
 function reset() {
-    interval = window.clearInterval(interval);
-    hour = 0;
-    minute = 0;
-    second = 0;
-    document.getElementById('timer').innerHTML="00:00:00";
-    document.getElementById('startStop').innerHTML='<i class="fas fa-play fa-2x"></i>';
+  interval = window.clearInterval(interval);
+  hour = 0;
+  minute = 0;
+  second = 0;
+  document.getElementById("timer").innerHTML = `00<span id="hour">H </span>00<span id="min" >M </span><span id="sec">00<span>S </span></span>`;
+  document.getElementById("startStop").innerHTML =
+    '<i class="fas fa-play fa-4x"></i>';
 }
